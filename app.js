93a4ef9edaffc63434ddcfd447f4fc6372ec2177
achrefmechergui <achreffcb123achref@gamil.com>
2025-03-23 21:39:55 +0100

    pt-1-done

--- app.js
+++ app.js
@@ -34,22 +34,53 @@
 // obj.gender = "male"
 
 ///// class declarartion :: 
-class Student {
-    constructor (name , birth){
-    this.firstName = name ; 
-    this.birthYear = birth ; 
-    ///// d'ont do that :: 
-    // this.calcAge = function (){
-    //     console.log(2025-this.birthYear)
-    // }
-    }
+// class Student {
+//     constructor (name , birth){
+//     this.firstName = name ; 
+//     this.birthYear = birth ; 
+//     ///// d'ont do that :: 
+//     // this.calcAge = function (){
+//     //     console.log(2025-this.birthYear)
+//     // }
+//     }
 
    
-    calcAge(){
-        console.log(2025-this.birthYear)
-    }
-}
+//     calcAge(){
+//         console.log(2025-this.birthYear)
+//     }
+// }
 
-const jhon = new Student ("jhon" , 2000) 
+// const jhon = new Student ("jhon" , 2000) 
 
-console.log(jhon)
+// console.log(jhon)
+
+
+
+///// challange correction :::: :  
+
+
+/////////methode2///////
+////// counstructor function ::: 
+// const Car = function (make, speed){
+//     this.make = make;
+//     this.speed = speed;
+// }
+// Car.prototype.accelerate = function () {
+//  this.speed = this.speed + 10 
+//  console.log(`the ${this.make} accelerate to ${this.speed} km/h`)
+// }
+// Car.prototype.break = function () {
+//     // this.speed = this.speed - 5 
+//     this.speed -= 5 
+//     console.log(`the ${this.make} slows down to ${this.speed} km/h`)
+// }
+// const car1 = new Car ('BMW', 120);  //// car1.speed =   (130) ; 
+// const car2 = new Car ('mercedes', 95);
+
+// console.log(car1)
+
+// car1.accelerate()
+// car1.accelerate()
+// car1.accelerate()
+// car1.break()
+// console.log(car1)
